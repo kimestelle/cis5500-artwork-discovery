79367b706ae1330349cb50acc04e8adef68c0623
kimestelle <137699667+kimestelle@users.noreply.github.com>
2025-11-18 00:53:30 -0500

    artists keywords

---
+++ database/functions.sql
@@ -0,0 +1,12 @@
+SELECT
+  Artwork.Title AS ArtworkTitle,
+  Artist.Name AS ArtistName,
+  HistoricalEvents.Title AS EventTitle,
+  HistoricalEvents.Location AS EventLocation,
+  HistoricalEvents.StartDate,
+  HistoricalEvents.EndDate
+FROM Artwork
+JOIN Artist ON Artwork.ArtistID = Artist.ArtistID
+JOIN HistoricalEvents ON Artist.Nationality = HistoricalEvents.Location
+ORDER BY Artist.Nationality, HistoricalEvents.StartDate;
+
