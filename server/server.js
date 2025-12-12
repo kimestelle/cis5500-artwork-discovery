bd017be5354f14533fbde2323763e662b35fb02f
kimestelle <137699667+kimestelle@users.noreply.github.com>
2025-12-11 22:20:05 -0500

    kfjgnd

--- server/server.js
+++ server/server.js
@@ -17,6 +17,14 @@ app.get('/bios/:museum', routes.bios);
 app.get('/artist_events', routes.artist_events);
 app.get('/artist_successors', routes.artist_successors);
 app.get('/event_artworks', routes.event_artworks);
+app.get('/similar_artists', routes.similar_artists);
+
+app.get('/artwork/:id', routes.artwork_detail);
+app.get('/artwork/:id/similar', routes.artwork_similar);
+
+app.get('/artist/:id', routes.artist_detail);
+app.get('/artist/:id/artworks', routes.artist_artworks);
+app.get('/artist/:id/similar', routes.artist_similar);
 
 app.listen(config.server_port, () => {
   console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
