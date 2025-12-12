cf8990709c078141907bc2da9cd461992250b006
kimestelle <137699667+kimestelle@users.noreply.github.com>
2025-12-11 22:37:33 -0500

    server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const config = require("./config");
+const { Pool } = require("pg");
 const routes = require("./routes");
+
+const config = new Pool({
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT || 5432),
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  ssl: { rejectUnauthorized: false },
+});
 
 const app = express();
 
