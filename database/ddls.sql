d36da1622605c40adbfb1fd9f05139053dc60c49
arushiga <aruagar1@gmail.com>
2025-12-12 18:41:53 -0500

    Replace Bios table with Bios4 table definition

--- database/ddls.sql
+++ database/ddls.sql
@@ -1,7 +1,5 @@
-CREATE TABLE Bios (
-    name VARCHAR(255) NOT NULL,
-    text VARCHAR(1000) 
+CREATE TABLE Bios4 (
+    name VARCHAR(510) NOT NULL,
+    text VARCHAR(5000) 
     PRIMARY KEY name
-    FOREIGN KEY name REFERENCES Artist(Name)
-
 )
